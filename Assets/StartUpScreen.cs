8e9a433ea021676df4fedc32bcb8be2ccad99028
Brexicity <32668336+Brexicity@users.noreply.github.com>
2018-11-12 16:23:51 +0100

    first touch test

---
+++ Assets/StartUpScreen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartUpScreen : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d"))
+        {
+          
+        }
+        if (Input.touchCount > 0)
+        {
+            Debug.Log(Input.touchCount);
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                Debug.Log("TEST");
+                SceneManager.LoadScene("PlayerCount");
+            }
+        }
+    }
+}
