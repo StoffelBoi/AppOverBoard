eb9b0bdd972619f1bca3a5f3958f25aa76789a10
StoffelBoi <stoff.joris@gmail.com>
2018-11-13 16:01:19 +0100

    fixes

--- Assets/StartUpScreen.cs
+++ Assets/StartUpScreen.cs
@@ -15,18 +15,17 @@ public class StartUpScreen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-                SceneManager.LoadScene("ConnectionScreen");
-=======
+            SceneManager.LoadScene("ConnectionScreen");
+        } 
         if (Input.touchCount > 0)
         {
             if (Input.GetTouch(0).phase == TouchPhase.Ended)
             {
                 SceneManager.LoadScene("PlayerCount");
             }
->>>>>>> 96d2ba2af1a234019317f93b29ae05384d60fd1c
         }
         //if (Input.touchCount > 0)
         //{
